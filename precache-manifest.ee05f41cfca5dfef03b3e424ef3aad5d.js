b1a8983d4b092ecd6f7425a6640784289a1db6d9
TokiLithuwix <loremlisumdol@gmail.com>
2022-08-02 15:58:40 +0700

    Updates

--- precache-manifest.ee05f41cfca5dfef03b3e424ef3aad5d.js
+++ precache-manifest.ee05f41cfca5dfef03b3e424ef3aad5d.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9eb19bae904bdf2b6f924e7d45520df8",
+    "revision": "e6a53a01910d1e363c7d331c6ff7a486",
     "url": "/simple-react-training/index.html"
   },
   {
-    "revision": "d7f571348be2e08b5f19",
-    "url": "/simple-react-training/static/css/main.afe05569.chunk.css"
+    "revision": "71dff35ecbb5640ec3ae",
+    "url": "/simple-react-training/static/css/main.859d9cef.chunk.css"
   },
   {
     "revision": "bb1f4ec887ad70eac80c",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/simple-react-training/static/js/2.4099b232.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d7f571348be2e08b5f19",
-    "url": "/simple-react-training/static/js/main.e0290ab2.chunk.js"
+    "revision": "71dff35ecbb5640ec3ae",
+    "url": "/simple-react-training/static/js/main.a6785c79.chunk.js"
   },
   {
     "revision": "5f791077e97f41f87a4f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/simple-react-training/precache-manifest.9b581b4b0982d76c7c8ec7037c7a0c8f.js"
+  "/simple-react-training/precache-manifest.ee05f41cfca5dfef03b3e424ef3aad5d.js"
 );
 
 self.addEventListener('message', (event) => {
