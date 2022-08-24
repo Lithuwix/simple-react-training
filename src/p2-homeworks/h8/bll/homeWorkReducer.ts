74c8bba44e5e37ffd561d4a402b69ba4c8c3c099
TokiLithuwix <loremlisumdol@gmail.com>
2022-08-24 07:20:07 +0700

    hw 8 is done

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,42 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+type sortSettingsType = 'up' | 'down'
+
+export const homeWorkReducer = (state: UserType[], action: allActionsType): UserType[] => {
     switch (action.type) {
         case 'sort': {
-            // need to fix
+            let newState = [...state]
+            if (action.payload === 'up') {
+                return newState.sort((a, b) => b.age - a.age)
+            }
+            if (action.payload === 'down') {
+                return newState.sort((a, b) => a.age - b.age)
+            }
             return state
         }
-        case 'check': {
-            // need to fix
-            return state
+        case 'check-age': {
+            return state.filter((p)=> p.age > action.payload)
         }
         default: return state
     }
+}
+
+type allActionsType = descendingAndAscendingSortACType | checkAgeACType
+
+type descendingAndAscendingSortACType = ReturnType<typeof descendingAndAscendingSortAC>
+
+const descendingAndAscendingSortAC = (settings: sortSettingsType) => {
+    return {
+        type: 'sort',
+        payload: settings
+    } as const
+}
+
+type checkAgeACType = ReturnType<typeof checkAgeAC>
+
+const checkAgeAC = (checkAge: number) => {
+    return {
+        type: 'check-age',
+        payload: checkAge
+    } as const
 }
